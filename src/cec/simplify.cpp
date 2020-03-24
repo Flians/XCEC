7b36199912c2700dc2296af466d82264b7226693
dcsgman <rongliangfu@outlook.com>
2020-03-24 16:06:46 +0800

    clean bufs

--- src/cec/simplify.cpp
+++ src/cec/simplify.cpp
@@ -8,7 +8,8 @@ simplify::~simplify()
 {
 }
 
-bool simplify::replace_node_by_id(vector<node *> *nodes, node *new_node, int id){
+bool simplify::replace_node_by_id(vector<node *> *nodes, node *new_node, int id)
+{
     for (auto &node : *nodes)
     {
         if (id == node->id)
@@ -20,22 +21,28 @@ bool simplify::replace_node_by_id(vector<node *> *nodes, node *new_node, int id)
     return false;
 }
 
-void simplify::clean_wire_buf(vector<node *> *miter) {
+/**
+ * clean all wires and bufs
+ * from PIs to POs
+ */
+void simplify::clean_wire_buf(vector<node *> *miter)
+{
     if (!miter || miter->size() == 0)
         return;
     int len = miter->size();
-    for (int i=0; i < len; ++i)
+    for (int i = 0; i < len; ++i)
     {
         node *pi = miter->at(i);
-        // cout << pi->name << " "<< Str_Value[pi->cell] << endl;
-        // clean_wire_buf(pi->outs);
 
-        if (!(pi->outs)) {
+        if (!(pi->outs) || pi->outs->size() == 0)
+        {
             continue;
         }
-        
-        if (pi->cell == WIRE) {
-            if (pi->ins->size()!=1) {
+
+        if (pi->cell == WIRE || pi->cell == BUF)
+        {
+            if (pi->ins->size() != 1)
+            {
                 perror((pi->name + " WIRE have none or more one inputs in simplify.clean_wire_buf!").c_str());
                 exit(-1);
             }
@@ -48,15 +55,18 @@ void simplify::clean_wire_buf(vector<node *> *miter) {
                 vector<node *>::iterator temp_in_end = (*it)->ins->end();
                 while (temp_in != temp_in_end)
                 {
-                    if (pi == (*temp_in)) {
+                    if (pi == (*temp_in))
+                    {
                         (*temp_in) = tin;
                         break;
                     }
                     temp_in++;
                 }
-                if (temp_in != temp_in_end) {
+                if (temp_in != temp_in_end)
+                {
                     tin->outs->push_back(*it);
-                } else
+                }
+                else
                 {
                     perror(("There are some wrong in" + pi->name).c_str());
                     exit(-1);
@@ -66,7 +76,13 @@ void simplify::clean_wire_buf(vector<node *> *miter) {
             vector<node *>().swap(*(pi->outs));
             delete pi;
             clean_wire_buf(tin->outs);
-        } else
+        }
+        // this node has been visited.
+        else if (pi->cell != IN && pi->outs->at(0)->cell != WIRE)
+        {
+            continue;
+        }
+        else
         {
             clean_wire_buf(pi->outs);
         }
