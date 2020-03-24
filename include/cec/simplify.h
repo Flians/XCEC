7891904015e2f86105c9e4a070ec554baef3876e
dcsgman <rongliangfu@outlook.com>
2020-03-24 16:06:46 +0800

    clean bufs

--- include/cec/simplify.h
+++ include/cec/simplify.h
@@ -14,6 +14,7 @@ public:
     // replace the node from vector<node *> *nodes with the new_node, which the id of this node is id
     bool replace_node_by_id(vector<node *> *nodes, node *new_node, int id);
 
+    // clean all wires and bufs from PIs to POs
     void clean_wire_buf(vector<node *> *);
 };
 
