a02eb5a8b99bc86863b4c7e8a6a3a0bd4c0ef64e
Flians <rongliangfu@outlook.com>
2020-07-15 09:10:56 -0400

    add result_16_debian_no_point_failed_11

--- main.cpp
+++ main.cpp
@@ -64,7 +64,8 @@ int main(int argc, char *argv[])
         /* evaluate the graph */
         cec cec_(argv[3]);
         // cec_.evaluate_from_PIs_to_POs(PIs);
-        cec_.evaluate_by_z3(layers, pre_time > 112000 ? 1700000 - pre_time : 16 * pre_time);
+        // cec_.evaluate_by_z3(layers, pre_time > 112000 ? 1700000 - pre_time : 16 * pre_time);
+        cec_.evaluate_by_z3(layers, 1000000);
         endTime = clock();
         cout << "The run time is: " << (double)(endTime - startTime) / CLOCKS_PER_SEC << " S" << endl;
         /* free up space */
