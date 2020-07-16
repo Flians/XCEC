1314781be3e57f2ef388720898d21278de1dc84c
Flians <rongliangfu@outlook.com>
2020-07-16 17:31:20 +0800

    update cryptominisat5

--- lib/cmake/cryptominisat5/cryptominisat5Targets-relwithdebinfo.cmake
+++ lib/cmake/cryptominisat5/cryptominisat5Targets-relwithdebinfo.cmake
@@ -7,13 +7,24 @@ set(CMAKE_IMPORT_FILE_VERSION 1)
 
 # Import target "cryptominisat5" for configuration "RelWithDebInfo"
 set_property(TARGET cryptominisat5 APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
-set_target_properties(cryptominisat5 PROPERTIES
-  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/mac/libcryptominisat5.5.7.dylib"
-  IMPORTED_SONAME_RELWITHDEBINFO "@rpath/libcryptominisat5.5.7.dylib"
-  )
+if(WINDOWS)
 
-list(APPEND _IMPORT_CHECK_TARGETS cryptominisat5 )
-list(APPEND _IMPORT_CHECK_FILES_FOR_cryptominisat5 "${_IMPORT_PREFIX}/lib/mac/libcryptominisat5.5.7.dylib" )
+elseif(LINUX)
+  set_target_properties(cryptominisat5 PROPERTIES
+    IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C;CXX"
+    IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/linux/libcryptominisat5.a"
+    )
 
+  list(APPEND _IMPORT_CHECK_TARGETS cryptominisat5 )
+  list(APPEND _IMPORT_CHECK_FILES_FOR_cryptominisat5 "${_IMPORT_PREFIX}/lib/linux/libcryptominisat5.a" )
+elseif(MACOS)
+  set_target_properties(cryptominisat5 PROPERTIES
+    IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/mac/libcryptominisat5.5.7.dylib"
+    IMPORTED_SONAME_RELWITHDEBINFO "@rpath/libcryptominisat5.5.7.dylib"
+    )
+
+  list(APPEND _IMPORT_CHECK_TARGETS cryptominisat5 )
+  list(APPEND _IMPORT_CHECK_FILES_FOR_cryptominisat5 "${_IMPORT_PREFIX}/lib/mac/libcryptominisat5.5.7.dylib" )
+endif()
 # Commands beyond this point should not need to know the version.
 set(CMAKE_IMPORT_FILE_VERSION)
