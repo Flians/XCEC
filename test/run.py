2a75abd25b2282ef078dd82851a73baeb06ade86
Flians <rongliangfu@outlook.com>
2020-08-20 09:28:57 -0400

    update stp

--- test/run.py
+++ test/run.py
@@ -1,9 +1,10 @@
 #!/usr/local/bin/python3
 import os
+import sys
 import subprocess
 
 
-def test_XCEC(exePath, root='./', output='./output/'):
+def test_XCEC(exePath, smt='stp', root='./', output='./output/'):
     files = os.listdir(os.path.join(root, 'cases'))
     files.sort(key= lambda x:int(x[4:]))
     if not os.path.exists(os.path.join(root, 'log')):
@@ -11,7 +12,7 @@ def test_XCEC(exePath, root='./', output='./output/'):
     for i, dir in enumerate(files):
         with open(os.path.join(root, 'log/'+dir+'.txt'), 'w', encoding='utf8') as log:
             cmd = [exePath, os.path.join(root, 'cases', dir, 'gf.v'), os.path.join(
-                root, 'cases', dir, 'rf.v'), os.path.join(output, 'output_' + dir + '.txt')]
+                root, 'cases', dir, 'rf.v'), os.path.join(output, 'output_' + dir + '.txt'), smt]
             p = subprocess.Popen(cmd, shell=False, bufsize=0,
                                  stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
             while p.poll() is None:
@@ -24,4 +25,7 @@ def test_XCEC(exePath, root='./', output='./output/'):
                 print(dir + ' failed!')
 
 if __name__ == "__main__":
-    test_XCEC('../build/XCEC')
+    smt = 'stp'
+    if len(sys.argv) == 2:
+        smt = sys.argv[1]
+    test_XCEC('../build/XCEC', smt)
